20ad5a84abf86b136f652f63c6c39513dfd3febf
Nick Williams <5554798+metanoic@users.noreply.github.com>
2018-07-10 00:51:08 -0500

    #17 #9 Connection and Device instantiate to idle states.  Explicit calls to Connect/Disconnect are now required.

---
+++ ODriveClientLibrary/DeviceStatusEnum.cs
@@ -0,0 +1,13 @@
+﻿namespace ODrive
+{
+    public enum DeviceStatus
+    {
+        Unknown = 0,
+        Initializing = 1,
+        Connecting = 2,
+        Ready = 3,
+        Faulted = 4,
+        Disposed = 5,
+        Disconnected = 6
+    }
+}
