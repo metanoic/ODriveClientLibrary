cbeb0d3d842b8e5e2f36d6b66e9e7ba03abf941f
Nick Williams <5554798+metanoic@users.noreply.github.com>
2018-07-15 05:13:25 -0500

    put program.cs back to useful state

--- Demo.ReadSetProperties/Program.cs
+++ Demo.ReadSetProperties/Program.cs
@@ -22,38 +22,29 @@
 
             if (foundDevice == null)
             {
-                throw new Exception("Could not find any suitable devices to connect to.");
+                throw new Exception("Could not find any suitable devices to connect to");
             }
 
             using (var oDrive = new Device(foundDevice))
             {
                 oDrive.Connect();
 
-                try
+                Console.WriteLine($"Firmware Version: {oDrive.FwVersionMajor}.{oDrive.FwVersionMinor}.{oDrive.FwVersionRevision}.{oDrive.FwVersionUnreleased}");
+                Console.WriteLine($"Hardware Version: {oDrive.HwVersionMajor}.{oDrive.HwVersionMinor}.{oDrive.HwVersionVariant}");
+                Console.WriteLine($"Serial Number: {(oDrive.SerialNumber.ToString("X2"))}");
+                Console.WriteLine($"Bus Voltage: {oDrive.VbusVoltage}V");
+
+                Console.WriteLine("Press any key to begin calibration...");
+                Console.ReadKey();
+
+                // Casting to byte is totally hackish.  Will improve when schema is improved or better partials with generated code.
+                oDrive.Axis0.RequestedState = (byte)AxisState.AXIS_STATE_FULL_CALIBRATION_SEQUENCE;
+
+                while (oDrive.Axis0.CurrentState != (byte)AxisState.AXIS_STATE_IDLE)
                 {
-                    var x = await oDrive.FetchSchema();
+                    System.Threading.Thread.Sleep(500);
+                    Application.DoEvents();
                 }
-                catch (Exception ex)
-                {
-                    var zz = 1;
-                }
-
-                //Console.WriteLine($"Firmware Version: {oDrive.FwVersionMajor}.{oDrive.FwVersionMinor}.{oDrive.FwVersionRevision}.{oDrive.FwVersionUnreleased}");
-                //Console.WriteLine($"Hardware Version: {oDrive.HwVersionMajor}.{oDrive.HwVersionMinor}.{oDrive.HwVersionVariant}");
-                //Console.WriteLine($"Serial Number: {(oDrive.SerialNumber.ToString("X2"))}");
-                //Console.WriteLine($"Bus Voltage: {oDrive.VbusVoltage}V");
-
-                //Console.WriteLine("Press any key to begin calibration...");
-                //Console.ReadKey();
-
-                //// Casting to byte is totally hackish.  Will improve when schema is improved or better partials with generated code.
-                //oDrive.Axis0.RequestedState = (byte)AxisState.AXIS_STATE_FULL_CALIBRATION_SEQUENCE;
-
-                //while (oDrive.Axis0.CurrentState != (byte)AxisState.AXIS_STATE_IDLE)
-                //{
-                //    System.Threading.Thread.Sleep(500);
-                //    Application.DoEvents();
-                //}
 
                 Console.WriteLine("Calibration complete");
 
